b4043ffecff3fa1b249bf69f2b935adc211c4327
TuongTLC <53476330+TuongTLC@users.noreply.github.com>
2023-02-08 11:40:31 +0700

    Update image APIs

---
+++ GreeenGarden.Business/Service/ImageService/IImageService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using GreeenGarden.Data.Models.ResultModel;
+using Microsoft.AspNetCore.Http;
+
+namespace GreeenGarden.Business.Service.ImageService
+{
+	public interface IImageService
+	{
+        public Task<ResultModel> UploadImage(IList<IFormFile> files);
+        public Task<ResultModel> DeleteImages(List<string> fileURLs);
+    }
+}
+
--- GreeenGarden.Business/Service/SecretService/SecretService.cs
+++ GreeenGarden.Business/Service/SecretService/SecretService.cs
@@ -63,6 +63,34 @@ namespace GreeenGarden.Business.Service.SecretService
 
             return tokenSecret;
         }
+        public static string GetIMGConn()
+        {
+            string tokenSecret = null;
+            try
+            {
+                SecretClientOptions options = new SecretClientOptions()
+                {
+                    Retry =
+                    {
+                        Delay= TimeSpan.FromSeconds(2),
+                        MaxDelay = TimeSpan.FromSeconds(16),
+                        MaxRetries = 5,
+                        Mode = RetryMode.Exponential
+                    }
+                };
+                var client = new SecretClient(new Uri("https://ggkeyvault2023.vault.azure.net/"), new DefaultAzureCredential(), options);
+
+                KeyVaultSecret secret = client.GetSecret("ImgConnection");
+
+                tokenSecret = secret.Value;
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+            }
+
+            return tokenSecret;
+        }
     }
 }
 
