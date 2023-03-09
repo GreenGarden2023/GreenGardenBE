50036c2791f6f8dbc5344c989c93aa1e82071fef
TuongTLC <trinhtuong98@gmail.com>
2023-03-10 00:22:23 +0700

    Revert "Update ProdItem"
    This reverts commit f49accc1e66a83848e674ee5a9029b3d42170edd.

--- GreeenGarden.Data/Entities/TblSize.cs
+++ GreeenGarden.Data/Entities/TblSize.cs
@@ -5,14 +5,8 @@ public partial class TblSize
     public Guid Id { get; set; }
 
     public string Name { get; set; } = null!;
-<<<<<<< HEAD
 
     public bool? Type { get; set; }
 
-=======
-
-    public bool? Type { get; set; }
-
->>>>>>> df73b02 (Update dbcontext)
     public virtual ICollection<TblSizeProductItem> TblSizeProductItems { get; } = new List<TblSizeProductItem>();
 }
