6e078bf20376e1973f60c70e9f48a2bb4cc439d7
melloDr <thanhlong020920@gmail.com>
2023-05-23 01:43:10 +0700

    Commit GetAllTakecreComboOrderForTech

--- GreeenGarden.Data/Repositories/TakecareComboOrderRepo/ITakecareComboOrderRepo.cs
+++ GreeenGarden.Data/Repositories/TakecareComboOrderRepo/ITakecareComboOrderRepo.cs
@@ -3,6 +3,7 @@ using EntityFrameworkPaginateCore;
 using GreeenGarden.Data.Entities;
 using GreeenGarden.Data.Models.PaginationModel;
 using GreeenGarden.Data.Models.ResultModel;
+using GreeenGarden.Data.Models.TakecareComboOrder;
 using GreeenGarden.Data.Repositories.GenericRepository;
 
 namespace GreeenGarden.Data.Repositories.TakecareComboOrderRepo
@@ -10,6 +11,7 @@ namespace GreeenGarden.Data.Repositories.TakecareComboOrderRepo
 	public interface ITakecareComboOrderRepo : IRepository<TblTakecareComboOrder>
     {
 		Task<Page<TblTakecareComboOrder>> GetAllTakecreComboOrder(PaginationRequestModel paginationRequestModel, string status);
+		Task<Page<TblTakecareComboOrder>> GetAllTakecreComboOrderForTech(PaginationRequestModel paginationRequestModel, TakecareComboOrderTechnicianReqModel model);
         Task<bool> ChangeTakecareComboOrderStatus(Guid id, string status);
         Task<ResultModel> UpdateOrderDeposit(Guid orderID);
         Task<ResultModel> UpdateOrderRemain(Guid orderID, double payAmount);
