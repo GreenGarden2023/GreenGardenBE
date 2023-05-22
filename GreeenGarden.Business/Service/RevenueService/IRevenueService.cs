ca3064685df446dbf025224fe92dac4c0fbae2ad
melloDr <thanhlong020920@gmail.com>
2023-05-23 01:15:16 +0700

    Commit getTransaction for combo

---
+++ GreeenGarden.Business/Service/RevenueService/IRevenueService.cs
@@ -0,0 +1,14 @@
+﻿using GreeenGarden.Data.Models.ResultModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreeenGarden.Business.Service.RevenueService
+{
+    public interface IRevenueService
+    {
+       // Task<ResultModel> GetCart(string token);
+    }
+}
